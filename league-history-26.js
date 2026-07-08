dd8414a0c55874ffc6fe92e32e368f905d82c9c5
stogz <sam.m.stogsdill@gmail.com>
2026-07-08 14:21:44 -0500

    Add 2011-12 NBA season data

--- league-history-26.js
+++ league-history-26.js
@@ -14,4 +14,14 @@ window.EGE_LEAGUE_HISTORY = [
     champion:   'Dallas Mavericks',
     finals_mvp: 'Dirk Nowitzki',        finals_mvp_team: 'Dallas Mavericks', finals_mvp_headshot: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1717.png',
   },
+    {
+    season: '2011-12',
+    mvp:        'LeBron James',       mvp_team:       'Miami Heat', mvp_headshot:       'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+    roty:       'Kyrie Irving',          roty_team:      'Cleveland Cavaliers',      roty_headshot:      'https://cdn.nba.com/headshots/nba/latest/1040x760/202681.png',
+    dpoy:       'Tyson Chandler',       dpoy_team:      'New York Knicks',     dpoy_headshot:      'https://cdn.nba.com/headshots/nba/latest/1040x760/2199.png',
+    mip:        'Ryan Anderson',         mip_team:       'Orlando Magic',         mip_headshot:       'https://cdn.nba.com/headshots/nba/latest/1040x760/201583.png',
+    sixmoty:    'James Harden',    sixmoty_team:   'Oklahoma City Thunder',       sixmoty_headshot:   'https://cdn.nba.com/headshots/nba/latest/1040x760/201935.png',
+    champion:   'Miami Heat',
+    finals_mvp: 'LeBron James',        finals_mvp_team: 'Miami Heat', finals_mvp_headshot: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+  },
 ];
